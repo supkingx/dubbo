fa04582865c15f030a7977d66322bff07a39c80b
wangchao <wangchao20@shein.com>
2023-01-02 23:10:36 +0800

    RoundRobinLoadBalance增加注释

--- dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
+++ dubbo-cluster/src/main/java/org/apache/dubbo/rpc/cluster/loadbalance/RoundRobinLoadBalance.java
@@ -66,6 +66,13 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
         }
     }
 
+    /**
+     * 双层map
+     * 外层map的key 是 全限定方法名
+     * 内层map的key 是 invoker 的 url： dubbo://ip:port/接口名
+     * 内层map的value 为轮询权重实例
+     * 内存map 表示某一个方法的所有invoker及其对应的轮询权重
+     */
     private ConcurrentMap<String, ConcurrentMap<String, WeightedRoundRobin>> methodWeightMap = new ConcurrentHashMap<String, ConcurrentMap<String, WeightedRoundRobin>>();
 
     /**
@@ -88,27 +95,41 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
 
     @Override
     protected <T> Invoker<T> doSelect(List<Invoker<T>> invokers, URL url, Invocation invocation) {
+        // 获取当前请求的方法的全限定方法名
         String key = invokers.get(0).getUrl().getServiceKey() + "." + invocation.getMethodName();
+        // 获取指定方法名的所有invoker 及其轮询权重，即内层map
         ConcurrentMap<String, WeightedRoundRobin> map = methodWeightMap.computeIfAbsent(key, k -> new ConcurrentHashMap<>());
         int totalWeight = 0;
+        // 记录最大的轮询权重值
         long maxCurrent = Long.MIN_VALUE;
+        // 获取当前时间
         long now = System.currentTimeMillis();
         Invoker<T> selectedInvoker = null;
         WeightedRoundRobin selectedWRR = null;
+
+        // 遍历所有的invoker，找到轮询权重最大的invoker及其轮询权重实例，和所有invoker主机权重之和
         for (Invoker<T> invoker : invokers) {
+            // 遍历当前遍历的invoker的url，即内层map的key
             String identifyString = invoker.getUrl().toIdentityString();
             int weight = getWeight(invoker, invocation);
+            // 获取 invoker 对应的权重实例
             WeightedRoundRobin weightedRoundRobin = map.computeIfAbsent(identifyString, k -> {
                 WeightedRoundRobin wrr = new WeightedRoundRobin();
                 wrr.setWeight(weight);
                 return wrr;
             });
 
+            // 若从invoker获取的主机权重与缓存中获取的主机权重不同
+            // 更新当前轮询权重的主机权重值
+            // 在warmup过程中，主机权重会发生变化
             if (weight != weightedRoundRobin.getWeight()) {
                 //weight changed
+                // map中的权重发生了变化，则使用新的权重
                 weightedRoundRobin.setWeight(weight);
             }
+            // 轮询权重加权
             long cur = weightedRoundRobin.increaseCurrent();
+            // 记录加权时间
             weightedRoundRobin.setLastUpdate(now);
             if (cur > maxCurrent) {
                 maxCurrent = cur;
@@ -117,9 +138,14 @@ public class RoundRobinLoadBalance extends AbstractLoadBalance {
             }
             totalWeight += weight;
         }
+
+        // invokers.size() 真正的invoker数量
+        // map.size() 从缓存中获取的invoker数量
+        // 一般只会存在 invokers.size() 的数量 < map.size()，例如：invoker宕机了，此时需要将map中长时间没有更新的invoker剔除掉
         if (invokers.size() != map.size()) {
             map.entrySet().removeIf(item -> now - item.getValue().getLastUpdate() > RECYCLE_PERIOD);
         }
+        // 找到轮询权重最大的invoker之后，将其轮询权重减 权重之和，并返回这个invoker
         if (selectedInvoker != null) {
             selectedWRR.sel(totalWeight);
             return selectedInvoker;
